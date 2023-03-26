15b9c8894448881f37ade64435b5ae2f9518e6bb
Hazem ABDELKAWY <hazem.abdelkawy@toyota-europe.com>
2023-03-26 11:21:54 +0200

    add hadith raw data and preprocessing scripts

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from src.scraper import HadithScraper
 from src.config import OPENAI_KEY
 
 if __name__ == '__main__':
-    base_url = 'https://sunnah.com/bukhari'
-    scraper = HadithScraper(base_url, OPENAI_KEY, data_dir="./Bukhari")
+    base_url = 'https://sunnah.com/ahmad'
+    scraper = HadithScraper(base_url, OPENAI_KEY, data_dir="./data/Musnad Ahmad")
     books_data = scraper.scrape_books()
